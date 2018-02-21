2a0b932c822b9bec9e0a764a3ede3a6150758af1
matheswaaran <matheswaarans@gmail.com>
2018-02-21 13:54:12 +0530

    Added: PHP Login and Register

---
+++ php/login.php
@@ -0,0 +1,31 @@
+<?php
+	include "includes/config.php";
+	include "includes/sessionUtils.php";
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		$session = new sessionUtils();
+		$login_username = $_POST["login_username"];
+		$login_password = $_POST["login_password"];
+//		$login_password = $session->encryptIt($login_password);
+		$login_error = "";
+		
+		$login_query = "SELECT * FROM users WHERE username = '$login_username' AND password = '$login_password'";
+		
+		$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_BASE) or die("Cannot Connect...");
+		
+		if (mysqli_query($db,$login_query)){
+			$login_res = mysqli_query($db,$login_query);
+			if (mysqli_num_rows($login_res) == 1){
+				$login_arr = mysqli_fetch_array($login_res);
+				$session->UserLogin($login_arr['uid'],$login_arr['email']);
+				header("location: ../home.php");
+			}else{
+				echo '<script> alert("Invalid credentials");</script>';
+				echo '<script> window.location="../index.html"; </script>';
+			}
+		}else{
+			echo '<script> alert("Login Error. Please Try Again.");</script>';
+			echo '<script> window.location="../index.html"; </script>';
+		}
+	}
+?>
---
+++ php/register.php
@@ -0,0 +1,33 @@
+<?php
+	include "includes/config.php";
+	include "includes/sessionUtils.php";
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		
+		$session = new sessionUtils();
+		
+		$reg_username= $_POST["reg_username"];
+		$reg_email=$_POST["reg_email"];
+		$reg_password=$_POST["reg_password"];
+//		$reg_password = $session->encryptIt($reg_password);
+		
+		$select_query = "SELECT * FROM users WHERE username = $reg_username";
+		
+		$register_query = "INSERT INTO users(username,password,email) VALUES ('$reg_username','$reg_email','$reg_password')";
+		
+		$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_BASE) or die("Cannot Connect...");
+
+        if (mysqli_num_rows(mysqli_query($db,$select_query)) == 0){
+            if (mysqli_query($db,$register_query)){
+                echo '<script> alert("User registered successfully.");</script>';
+                echo '<script> window.location="../index.html"; </script>';
+            }else{
+                echo '<script> alert("User registration falied.");</script>';
+                echo '<script> window.location="../index.html"; </script>';
+            }
+        }else{
+            echo '<script> alert("User already exists.");</script>';
+            echo '<script> window.location="../index.html"; </script>';
+        }
+	}
+?>
