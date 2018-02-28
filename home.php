43ef23edae8f711f286ea4e50378a9e3ce62dca7
matheswaaran <matheswaarans@gmail.com>
2018-02-28 13:01:31 +0530

    Added: Page Banners Chnaged images

--- home.php
+++ home.php
@@ -57,17 +57,14 @@
 			<section  class="homepage-slider" id="home-slider">
 				<div class="flexslider">
 					<ul class="slides">
-						<li>
-							<img src="themes/images/carousel/banner-1.jpg" alt="" />
-						</li>
-						<li>
-							<img src="themes/images/carousel/banner-2.jpg" alt="" />
-							<div class="intro">
-								<h1>Mid season sale</h1>
-								<p><span>Up to 50% Off</span></p>
-								<p><span>On selected items online and in stores</span></p>
-							</div>
-						</li>
+						<li><img src="themes/images/carousel/001.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/002.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/003.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/004.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/005.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/006.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/007.jpg" alt="" /></li>
+						<li><img src="themes/images/carousel/008.jpg" alt="" /></li>
 					</ul>
 				</div>			
 			</section>
